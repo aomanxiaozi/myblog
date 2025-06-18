44a9310cd4e9d75d37f944e357a5b6425e9f655b
aomanxiaozi <1215697142@qq.com>
2025-06-18 08:41:53 +0800

    css3

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,11 +1,53 @@
 <template>
+    <div class="home">
 <p>Home</p>
+<button @click="handleClick">点击</button>
+<p>当前计数：{{ count }}</p>
+<div class="element">hello,this is a element</div>
+<div class="element2">hello,this is a element2</div>
+<div class="element3">hello,this is a element3</div>
+</div>
 </template>
 <script setup>
-
+import { ref } from 'vue'
+const count = ref(0)
+const handleClick = () => {
+    count.value++
+}
 
 </script>
 
 <style scoped>  
+.home{
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   height: 100vh;
+}
+.element{
+    width: 100px;
+    height: 100px;
+    background-color: #f00;
+    transform: rotate(45deg);
+}
+.element2{
+    width: 50px;
+    height: 50px;
+    background-color: #0f0;
 
+}
+.element3{
+    width: 100px;
+    height: 100px;
+    background-color: #00f;
+    transition: transform 2s;
+}
+.element3:hover{
+    width: 100px;
+    height: 100px;
+    background-color: #00f;
+    transform:rotateX(45deg);
+ 
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-const routes = [
-    {
-        path: '/home',
-        component: () => import('@/views/Home.vue')
-    },
-    {
-        path: '/',
-        redirect: '/home'
-    }
-]
+
 
 const router =createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes
+    routes:[
+        {
+            path: '/home',
+            name: 'home',
+            component: () => import('../components/Home.vue')
+        },
+        {
+            path: '/',
+            redirect: '/home'
+        }
+    ]
 })
 
 export default router
